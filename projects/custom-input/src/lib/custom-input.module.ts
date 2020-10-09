853e74edcb8cabf3c61ee12cc6774fe15a8ffc8d
Erika Vacacela <erikavacacelaeb@gmail.com>
2020-10-09 02:16:41 -0500

    Added project conf

---
+++ projects/custom-input/src/lib/custom-input.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CustomInputComponent } from './custom-input.component';
+
+
+
+@NgModule({
+  declarations: [CustomInputComponent],
+  imports: [
+  ],
+  exports: [CustomInputComponent]
+})
+export class CustomInputModule { }
